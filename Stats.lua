ca2f2a8f79a6aba3f632356717706f03437dd29b
Nocrazypc <nocrazypc@hotmail.com>
2024-11-15 07:57:29 -0300

    Update Stats.lua

--- Stats.lua
+++ Stats.lua
@@ -40,7 +40,7 @@ MainFrame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 MainFrame.BackgroundTransparency = 1.000
 MainFrame.BorderColor3 = Color3.fromRGB(0, 0, 0)
 MainFrame.BorderSizePixel = 0
-MainFrame.Position = UDim2.new(0.923189096, 0, 0.443002731, 0)
+MainFrame.Position = UDim2.new(0.933189096, 0, 0.443002731, 0)
 MainFrame.Size = UDim2.new(0.200000012, 0, 0.300000006, 0)
 MainFrame.Parent = StatsGui
 
