b52f6e2847862287b4c308f3b8c9e3290af29a0d
Nocrazypc <nocrazypc@hotmail.com>
2024-12-05 02:49:12 -0300

    Update Tele.lua

--- Tele.lua
+++ Tele.lua
@@ -56,8 +56,8 @@ function Teleport.PlaceFloorAtFarmingHome()
 	SurfaceGui.Face = Enum.NormalId.Top
 
 	TextLabel.Parent = SurfaceGui
-	TextLabel.BackgroundColor3 = Color3.fromRGB(50, 180, 0)
-	TextLabel.BackgroundTransparency = 0.200
+	TextLabel.BackgroundColor3 = Color3.fromRGB(30, 180, 0)
+	TextLabel.BackgroundTransparency = 0.250
 	TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
 	TextLabel.BorderSizePixel = 0
 	TextLabel.Size = UDim2.new(1, 0, 1, 0)
