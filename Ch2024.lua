2ddd9200d2cbf05e9129e4ee820e1580b417fcfb
Nocrazypc <nocrazypc@hotmail.com>
2024-12-21 09:47:16 -0300

    Update Ch2024.lua

--- Ch2024.lua
+++ Ch2024.lua
@@ -2,17 +2,15 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local Christmas2024 = {}
 
---[[function Christmas2024.getGingerbread()
+function Christmas2024.getGingerbread()
     local GingerbreadMarkers = ReplicatedStorage.Resources.IceSkating.GingerbreadMarkers
     for _, v in GingerbreadMarkers:GetChildren() do
-        if v:IsA("BasePart") then
+        if v:IsA("BasePart") and not ClientData.get_data()[localPlayer.Name].winter_2024_gingerbread_captured_list[v.Name] then
             ReplicatedStorage.API:FindFirstChild("WinterEventAPI/PickUpGingerbread"):InvokeServer(v.Name)
-            task.wait()
         end
     end
-    task.wait(1)
     ReplicatedStorage.API:FindFirstChild("WinterEventAPI/RedeemPendingGingerbread"):FireServer()
-end--]]
+end
 
 
 return Christmas2024
