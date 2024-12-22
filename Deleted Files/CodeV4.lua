c8623e682e9cad91a51fc70dd506c923839ed3d5
Nocrazypc <nocrazypc@hotmail.com>
2024-12-21 21:58:07 -0300

    Update CodeV4.lua

--- Deleted Files/CodeV4.lua
+++ Deleted Files/CodeV4.lua
@@ -1706,7 +1706,7 @@ if getgenv().AutoFCMinigame then
 	task.spawn(function()
 		while true do
                         getRewardFromAdventCalendar()
-                        task.wait(10)
+                        task.wait(12)
 			--print("running frostclaw minigame")
 			if FC2024.CreateAndStartLobby() then
 				FC2024.StartGame()
