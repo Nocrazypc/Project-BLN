5a22b3964f40651337cf09adcf21892e60398cff
Nocrazypc <nocrazypc@hotmail.com>
2025-01-01 09:34:55 -0300

    Update CodeV4.lua

--- Deleted Files/CodeV4.lua
+++ Deleted Files/CodeV4.lua
@@ -1056,11 +1056,11 @@ UserInputService.InputBegan:Connect(function(input, processed)
     end
 end)
 
-localPlayer.OnTeleport:Connect(function(State)
+--[[localPlayer.OnTeleport:Connect(function(State)
     if State == Enum.TeleportState.InProgress then
         game:Shutdown()
     end
-end)
+end)--]]
 
 WelcomeScreen = localPlayer.PlayerGui.DialogApp.Dialog.NormalDialog:GetPropertyChangedSignal('Visible'):Connect(function(
 )
