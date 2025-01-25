95213a2053869afc33dda9866f3c91c5bbdf02f0
Nocrazypc <nocrazypc@hotmail.com>
2025-01-24 21:12:17 -0300

    Add files via upload

---
+++ S25.lua
@@ -0,0 +1,23 @@
+ 
+local ReplicatedStorage = game:GetService('ReplicatedStorage')
+local Workspace = game:GetService('Workspace')
+local StarsFolder = Workspace:WaitForChild('Collectables')
+local LunarNewYear2025 = {}
+
+        function LunarNewYear2025:CollectStars()
+            for _, v in StarsFolder:GetChildren()do
+                if not v:IsA('Model') then
+                    continue
+                end
+                ReplicatedStorage.API['MoonAPI/ShootingStarCollected']:FireServer('MainMap', v.Name)
+                print(`Collected {v.Name} star`)
+                task.wait(1)
+            end
+        end
+        return LunarNewYear2025
+    end
+end
+
+
+
+
