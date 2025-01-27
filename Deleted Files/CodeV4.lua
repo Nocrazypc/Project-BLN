f8de46977fa3ebd12d46fb8cc63110d9fc901666
Nocrazypc <nocrazypc@hotmail.com>
2025-01-26 22:11:07 -0300

    Update CodeV4.lua

--- Deleted Files/CodeV4.lua
+++ Deleted Files/CodeV4.lua
@@ -688,21 +688,15 @@ end--]]
 local completeBabyAilments = function()
     for key, _ in ClientData.get_data()[localPlayer.Name].ailments_manager.baby_ailments do
         if key == 'hungry' then
-        --ReplicatedStorage.API['ToolAPI/Unequip']:InvokeServer(PetCurrentlyFarming, {})
-        --task.wait(1)
-        --ReplicatedStorage.API['ToolAPI/Equip']:InvokeServer(PetCurrentlyFarming, {})
-            ReplicatedStorage.API['ShopAPI/BuyItem']:InvokeServer('food', "icecream", {})
-            task.wait(5)
+            --ReplicatedStorage.API['ShopAPI/BuyItem']:InvokeServer('food', "icecream", {})
+            --task.wait(5)
             Ailments:BabyHungryAilment()
             task.wait(7)
 			
             return
         elseif key == 'thirsty' then
-        --ReplicatedStorage.API['ToolAPI/Unequip']:InvokeServer(PetCurrentlyFarming, {})
-        --task.wait(1)
-        --ReplicatedStorage.API['ToolAPI/Equip']:InvokeServer(PetCurrentlyFarming, {})
-            ReplicatedStorage.API['ShopAPI/BuyItem']:InvokeServer('food', "water", {})
-            task.wait(5)
+            --ReplicatedStorage.API['ShopAPI/BuyItem']:InvokeServer('food', "water", {})
+            --task.wait(5)
             Ailments:BabyThirstyAilment()
             task.wait(7)
 
@@ -769,22 +763,21 @@ local CompletePetAilments = function()
 
     for key, _ in ClientData.get_data()[localPlayer.Name].ailments_manager.ailments[petUnique]do
         if key == 'hungry' then
-        ReplicatedStorage.API['ToolAPI/Unequip']:InvokeServer(PetCurrentlyFarming, {})
-        task.wait(1)
-        ReplicatedStorage.API['ToolAPI/Equip']:InvokeServer(PetCurrentlyFarming, {})
-			
-            ReplicatedStorage.API['ShopAPI/BuyItem']:InvokeServer('food', "icecream", {})
-            task.wait(5)
+        --ReplicatedStorage.API['ToolAPI/Unequip']:InvokeServer(PetCurrentlyFarming, {})
+        --task.wait(1)
+        --ReplicatedStorage.API['ToolAPI/Equip']:InvokeServer(PetCurrentlyFarming, {})
+	    --ReplicatedStorage.API['ShopAPI/BuyItem']:InvokeServer('food', "icecream", {})
+            --task.wait(5)
             Ailments:HungryAilment()
             task.wait(2)
 
             return true
         elseif key == 'thirsty' then
-	ReplicatedStorage.API['ToolAPI/Unequip']:InvokeServer(PetCurrentlyFarming, {})
-        task.wait(1)
-        ReplicatedStorage.API['ToolAPI/Equip']:InvokeServer(PetCurrentlyFarming, {})
-            ReplicatedStorage.API['ShopAPI/BuyItem']:InvokeServer('food', "water", {})
-            task.wait(5)
+	--ReplicatedStorage.API['ToolAPI/Unequip']:InvokeServer(PetCurrentlyFarming, {})
+        --task.wait(1)
+        --ReplicatedStorage.API['ToolAPI/Equip']:InvokeServer(PetCurrentlyFarming, {})
+            --ReplicatedStorage.API['ShopAPI/BuyItem']:InvokeServer('food', "water", {})
+            --task.wait(5)
             Ailments:ThirstyAilment()
             task.wait(2)
 			
