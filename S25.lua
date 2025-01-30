5b03ab6788d9b26c11e3d7bbc67b8d6e3e9c8fd0
Nocrazypc <nocrazypc@hotmail.com>
2025-01-30 00:14:43 -0300

    Add files via upload

--- S25.lua
+++ S25.lua
@@ -1,22 +1,34 @@
- 
-local ReplicatedStorage = game:GetService('ReplicatedStorage')
-local Workspace = game:GetService('Workspace')
-local StarsFolder = Workspace:WaitForChild('Collectables')
+
+local RS = game:GetService("ReplicatedStorage")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ClientData = require(RS.ClientModules.Core.ClientData)
+local Player = game:GetService("Players").LocalPlayer
+local RouterClient = require(RS.ClientModules.Core:WaitForChild("RouterClient"):WaitForChild("RouterClient"))
 local LunarNewYear2025 = {}
 
-        function LunarNewYear2025:CollectStars()
-            for _, v in StarsFolder:GetChildren()do
-                if not v:IsA('Model') then continue
-                end
-                ReplicatedStorage.API['MoonAPI/ShootingStarCollected']:FireServer('MainMap', v.Name)
-                print(`Collected {v.Name} star`)
-                task.wait(1)
-            end
+for i, v in pairs(debug.getupvalue(RouterClient.init, 7)) do
+    v.Name = i
+end
+
+
+
+
+
+-- SHOOTING STARS
+function LunarNewYear2025:GetAvailableShootingStars(mapName)
+    return ClientData.get_data()[Player.Name].moon_2025_shooting_stars.available_stars[mapName]
+end
+
+function LunarNewYear2025:FetchStars(mapName)
+    while task.wait() and GetAvailableShootingStars(mapName) ~= 0 do
+        for i = 1, 150 do
+            RS.API:WaitForChild("MoonAPI/ShootingStarCollected"):FireServer(
+                mapName,
+                tostring(i)
+            )
+            task.wait()
         end
+    end
+end
+
 return LunarNewYear2025
-
-
-
-
-
-
