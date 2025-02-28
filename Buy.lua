e7adecfebc6f7f61a9b14693c3a745087230142c
Nocrazypc <nocrazypc@hotmail.com>
2025-02-28 01:07:57 -0300

    Update Buy.lua

--- Buy.lua
+++ Buy.lua
@@ -1,59 +1,109 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Player = game:GetService("Players").LocalPlayer
-
 local BuyItems = {}
-
 local ClientData = require(ReplicatedStorage:WaitForChild("ClientModules"):WaitForChild("Core"):WaitForChild("ClientData"))
 
--- local petsToBuy = {
---     {NameId = "halloween_2024_franken_feline", MaxAmount = 48},
---     {NameId = "halloween_2024_sea_skeleton_panda", MaxAmount = 48},
---     {NameId = "halloween_2024_indian_flying_fox", MaxAmount = 80},
---     {NameId = "halloween_2024_headless_horse", MaxAmount = 80},
---     {NameId = "halloween_2024_marabou_stork", MaxAmount = 16},
---     {NameId = "halloween_2024_scarebear", MaxAmount = 16}
--- }
 
-local function hasPetMaxAmount(nameId: string, maxAmount: number)
-    local count = 0
-    for _, pet in ClientData.get_data()[Player.Name].inventory.pets do
-        if nameId == pet.id then
-            count += 1
+        local function getCategory(nameId)
+            if typeof(nameId) ~= 'string' then
+                print(`{nameId} is not a string`)
+
+                return
+            end
+
+            for _, v in InventoryDB do
+                for key, value in v do
+                    if key == nameId then
+                        return value.category
+                    end
+                end
+            end
+
+            return nil
         end
-    end
-    if count < maxAmount then
-        return (maxAmount - count)
-    end
+        local function hasPetMaxAmount(nameId, maxAmount)
+            local category = getCategory(nameId)
 
-    return 0
-end
+            if not category then
+                return print('no category')
+            end
 
-local function buyPet(petNameId: string, howManyToBuy: number)
-    for i = 1, howManyToBuy do
-        local hasMoney = game.ReplicatedStorage.API["ShopAPI/BuyItem"]:InvokeServer("pets", petNameId, {})
-        if hasMoney == "too little money" then
-            return false
+            local count = 0
+
+            for _, pet in ClientData.get_data()[localPlayer.Name].inventory[category]do
+                if nameId == pet.id then
+                    count += 1
+                end
+            end
+
+            if count < maxAmount then
+                return (maxAmount - count)
+            end
+
+            return 0
         end
-        task.wait(.1)
-    end
-    return true
-end
+        local function buyPet(petNameId, howManyToBuy)
+            local category = getCategory(petNameId)
 
-function BuyItems:BuyPets(petsToBuy: table)
-    for _, myPet in ipairs(petsToBuy) do
-        local amount = hasPetMaxAmount(myPet.NameId, myPet.MaxAmount)
-        --print(amount)
-        if amount == 0 then
-            --print(`has max amount of: {myPet.NameId}, skipping`)
-            continue 
-        end
-        if not buyPet(myPet.NameId, amount) then
-            --print(`has no money to buy more so breaking out of loop`)
-            break 
-        end
-        --print(`bought all pets needed: {myPet.NameId}`)
-    end 
-end
+            if not category then
+                return print('no category')
+            end
 
+            for i = 1, howManyToBuy do
+                local hasMoney = ReplicatedStorage.API['ShopAPI/BuyItem']:InvokeServer(category, petNameId, {})
+
+                if hasMoney ~= 'success' then
+                    return false
+                end
+
+                task.wait(0.1)
+            end
+
+            return true
+        end
+
+        function BuyItems:BuyPets(petsToBuy)
+            for _, myPet in ipairs(petsToBuy)do
+                local amount = hasPetMaxAmount(myPet.NameId, myPet.MaxAmount)
+
+                print(amount)
+
+                if amount == 0 then
+                    print(`has max amount of: {myPet.NameId}, skipping`)
+
+                    continue
+                end
+                if not buyPet(myPet.NameId, amount) then
+                    print(`Has no money to buy more or something went wrong.`)
+
+                    break
+                end
+
+                print(`bought all pets needed: {myPet.NameId}`)
+            end
+        end
+
+        local function openBox(nameId)
+            local category = getCategory(nameId)
+
+            for _, v in ClientData.get_data()[localPlayer.Name].inventory[category]do
+                if v.id == nameId then
+                    ReplicatedStorage.API['LootBoxAPI/ExchangeItemForReward']:InvokeServer(v['id'], v['unique'])
+                    task.wait(0.1)
+                end
+            end
+        end
+
+        function BuyItems:OpenItems(nameIdTable)
+            if typeof(nameIdTable) ~= 'table' then
+                return print(`{nameIdTable} is not a table`)
+            end
+
+            for _, v in nameIdTable do
+                openBox(v)
+            end
+
+            return
+        end
 
 return BuyItems
