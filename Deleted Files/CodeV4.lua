682cb5d5b94b2f92fd04ff4e8bdac17e8c9a13d8
Nocrazypc <nocrazypc@hotmail.com>
2025-02-28 17:38:25 -0300

    Update CodeV4.lua

--- Deleted Files/CodeV4.lua
+++ Deleted Files/CodeV4.lua
@@ -866,13 +866,13 @@ local CompletePetAilments = function()
             return true
         end
     end
-    --[[for key, _ in ClientData.get_data()[localPlayer.Name].ailments_manager.ailments[petUnique]do
+    for key, _ in ClientData.get_data()[localPlayer.Name].ailments_manager.ailments[petUnique]do
         if key:match('mystery') then
             Ailments:MysteryAilment(key, petUnique)
 
             return true
         end
-    end--]]
+    end
 
     return false
 end
