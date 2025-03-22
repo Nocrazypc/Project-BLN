2942944429d6687418756e59273792ea19974d3a
Nocrazypc <nocrazypc@hotmail.com>
2025-03-22 06:24:58 -0300

    Add files via upload

--- Deleted Files/CodeV4.lua
+++ Deleted Files/CodeV4.lua
@@ -29,6 +29,7 @@ local Workspace = game:GetService('Workspace')
 local ReplicatedStorage = game:GetService('ReplicatedStorage')
 local RS = game:GetService("ReplicatedStorage")
 local UserInputService = game:GetService('UserInputService')
+local VirtualInputManager = game:GetService('VirtualInputManager')
 local Lighting = game:GetService('Lighting')
 local UserGameSettings = UserSettings():GetService('UserGameSettings')
 local VirtualUser = game:GetService('VirtualUser')
@@ -195,7 +196,8 @@ local pets_uncommon = {}
 local pets_common = {}
 local Pets_commonto_ultrarare = {}
 local pets_legendary_to_common = {}
-local fireButton = function(button)
+
+--[[local fireButton = function(button)
     local success, errorMessage = pcall(function()
         firesignal(button.MouseButton1Down)
         firesignal(button.MouseButton1Click)
@@ -203,7 +205,29 @@ local fireButton = function(button)
     end)
 
     print(success, errorMessage)
-end
+end--]]
+
+local function clickGuiButton(button, xOffset, yOffset)
+     if typeof(button) ~= 'Instance' then
+         return 
+     -- Misc.DebugModePrint('button is not a Instance')
+     end
+    local xOffset = xOffset or 60
+    local yOffset = yOffset or 60
+
+    task.wait()
+    VirtualInputManager:SendMouseButtonEvent(button.AbsolutePosition.X + xOffset, button.AbsolutePosition.Y + yOffset, 0, true, game, 1)
+    task.wait()
+    VirtualInputManager:SendMouseButtonEvent(button.AbsolutePosition.X + xOffset, button.AbsolutePosition.Y + yOffset, 0, false, game, 1)
+    task.wait()
+ 
+     return
+ end
+
+ local function fireButton(button)
+     clickGuiButton(button)
+ end
+	
 local findButton = function(text, dialogFramePassOn)
     task.wait()
 
