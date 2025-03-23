7a5be3f658f1ff360e615f128be36f3762344d4f
Nocrazypc <nocrazypc@hotmail.com>
2025-03-23 20:12:47 -0300

    Add files via upload

--- ID25.lua
+++ ID25.lua
@@ -1,8 +1,28 @@
+ 
         local ReplicatedStorage = game:GetService('ReplicatedStorage')
+        local Players = game:GetService('Players')
         local Bypass = require(ReplicatedStorage:WaitForChild('Fsys')).load
         local Workspace = game:GetService('Workspace')
         local SlipperyEvent = {}
 
+        function SlipperyEvent.CreatePlatform()
+            if Workspace:FindFirstChild('MinigamePlatform') then
+                return
+            end
+
+            local part = Instance.new('Part')
+
+            part.Position = Workspace.StaticMap.IceCubeHillMinigameStatic.HillOrientation.Position + Vector3.new(0, 15, 0)
+            part.Size = Vector3.new(200, 2, 200)
+            part.Anchored = true
+            part.Transparency = 1
+            part.Name = 'MinigamePlatform'
+            part.Parent = Workspace
+        end
+
+        local function teleportToPlatform()
+            Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Workspace.MinigamePlatform.CFrame + Vector3.new(0, 5, 0)
+        end
         local function IceCubeEvent()
             local iceCubeHillMinigameStatic = Workspace.StaticMap.IceCubeHillMinigameStatic
             local iceCubesFolder = iceCubeHillMinigameStatic:FindFirstChild('IceCubes')
@@ -10,35 +30,53 @@
             if not iceCubesFolder then
                 return
             end
-            
 
-           for i, v in ipairs(iceCubesFolder:GetChildren())do
+            for i, v in ipairs(iceCubesFolder:GetChildren())do
                 if not v then
                     continue
                 end
                 if not v.PrimaryPart then
                     continue
                 end
-                
+
+                local args1 = {
+                    [1] = 'ice_cube_hill_minigame',
+                    [2] = 'ice_cube_touched',
+                    [3] = v.Name,
+                }
+
+                ReplicatedStorage.API['MinigameAPI/MessageServer']:FireServer(unpack(args1))
+            end
+            for i, v in ipairs(iceCubesFolder:GetChildren())do
+                if not v then
+                    continue
+                end
+                if not v.PrimaryPart then
+                    continue
+                end
+
                 local args2 = {
                     [1] = 'ice_cube_hill_minigame',
                     [2] = 'attempt_hit',
                     [3] = v.Name,
-                    --[4] = v.PrimaryPart.Position,
-                    --[5] = math.random(800, 2000),
-                    --[6] = Bypass('LiveOpsTime').now(),
+                    [4] = v.PrimaryPart.Position,
+                    [5] = math.random(800, 2000),
+                    [6] = Bypass('LiveOpsTime').now(),
                 }
 
                 ReplicatedStorage.API['MinigameAPI/MessageServer']:FireServer(unpack(args2))
 
-                --v.DecalPart.Color = Color3.fromRGB(255, 0, 0)
+                v.DecalPart.Color = Color3.fromRGB(255, 0, 0)
 
-                task.wait(1)
+                task.wait(1.1)
+
                 break
             end
         end
 
         function SlipperyEvent.Start()
+            teleportToPlatform()
+
             if not Workspace:WaitForChild('StaticMap', 15) then
                 return
             end
@@ -49,14 +87,14 @@
                 return
             end
 
-            --print('\u{1f431}\u{200d}\u{1f4bb} STARTING MINIGAME \u{1f431}\u{200d}\u{1f4bb}')
+            print('\u{1f431}\u{200d}\u{1f4bb} STARTING MINIGAME \u{1f431}\u{200d}\u{1f4bb}')
 
             while Workspace.StaticMap.ice_cube_hill_minigame_minigame_state.is_game_active.Value do
                 IceCubeEvent()
                 task.wait()
             end
 
-            --print('\u{1f389} EVENT DONE \u{1f389}')
+            print('\u{1f389} EVENT DONE \u{1f389}')
         end
 
         return SlipperyEvent
