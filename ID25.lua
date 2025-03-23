2d88f5740dda6197cffd83cb8f37ed8892bba4e0
Nocrazypc <nocrazypc@hotmail.com>
2025-03-23 16:11:49 -0300

    Update ID25.lua

--- ID25.lua
+++ ID25.lua
@@ -11,40 +11,32 @@
                 return
             end
 
-            for i, v in ipairs(iceCubesFolder:GetChildren())do
+              
+-------------------------------------------------------------------------------------------
+            --[[for i, v in ipairs(iceCubesFolder:GetChildren())do
                 if not v then
                     continue
                 end
                 if not v.PrimaryPart then
                     continue
-                end
-
-                local args1 = {
-                    [1] = 'ice_cube_hill_minigame',
-                    [2] = 'ice_cube_touched',
-                    [3] = v.Name,
-                }
-
-                ReplicatedStorage.API['MinigameAPI/MessageServer']:FireServer(unpack(args1))
+                end--]]
                 
-                task.wait(0.2)
-
                 local args2 = {
                     [1] = 'ice_cube_hill_minigame',
                     [2] = 'attempt_hit',
-                    [3] = v.Name,
-                    [4] = v.PrimaryPart.Position,
-                    [5] = math.random(800, 2000),
-                    [6] = Bypass('LiveOpsTime').now(),
+                    --[3] = v.Name,
+                    --[4] = v.PrimaryPart.Position,
+                    --[5] = math.random(800, 2000),
+                    --[6] = Bypass('LiveOpsTime').now(),
                 }
 
                 ReplicatedStorage.API['MinigameAPI/MessageServer']:FireServer(unpack(args2))
 
-                v.DecalPart.Color = Color3.fromRGB(255, 0, 0)
+                --v.DecalPart.Color = Color3.fromRGB(255, 0, 0)
 
-                task.wait(0.2)
-
-                end
+                task.wait(1)
+                break
+            --end
         end
 
         function SlipperyEvent.Start()
@@ -58,14 +50,14 @@
                 return
             end
 
-            print('\u{1f431}\u{200d}\u{1f4bb} STARTING MINIGAME \u{1f431}\u{200d}\u{1f4bb}')
+            --print('\u{1f431}\u{200d}\u{1f4bb} STARTING MINIGAME \u{1f431}\u{200d}\u{1f4bb}')
 
             while Workspace.StaticMap.ice_cube_hill_minigame_minigame_state.is_game_active.Value do
                 IceCubeEvent()
-                task.wait(0.3)
+                task.wait()
             end
 
-            print('\u{1f389} EVENT DONE \u{1f389}')
+            --print('\u{1f389} EVENT DONE \u{1f389}')
         end
 
         return SlipperyEvent
