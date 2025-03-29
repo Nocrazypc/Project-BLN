8c80979b6517ad706a740526eb6ae180faef198a
Nocrazypc <nocrazypc@hotmail.com>
2025-03-29 00:43:24 -0300

    Update CodeV4.lua

--- Deleted Files/CodeV4.lua
+++ Deleted Files/CodeV4.lua
@@ -2403,6 +2403,9 @@ getgenv().PotionToggle = AgeUpPotionTab:CreateToggle({
     end,
 })
 
+
+localPlayer.PlayerGui.DialogApp:WaitForChild('Dialog'):WaitForChild('IceDimension2025Dialog').Visible = false
+
 --------------------update Stats UI ----------------
             while task.wait(5) do
 			StatsGuis:UpdateText("TimeFrame")
