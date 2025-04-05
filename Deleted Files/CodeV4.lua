b4ea816e3d4e5a0554606c6aad03cf01125d8016
Nocrazypc <nocrazypc@hotmail.com>
2025-04-05 07:28:26 -0300

    Update CodeV4.lua

--- Deleted Files/CodeV4.lua
+++ Deleted Files/CodeV4.lua
@@ -956,15 +956,10 @@ local autoFarm = function()
 			
         while true do
             if isInMiniGame then
-                local count = 0
-
                 repeat
-                    print(`\u{23f1}\u{fe0f} Waiting for 50 secs [inside minigame] \u{23f1}\u{fe0f}`)
-                    
-                    count = count + 50
-
+                    print(`\u{23f1}\u{fe0f} Waiting for 50 secs [inside minigame] \u{23f1}\u{fe0f}`)               
                     task.wait(50)
-                    until not isInMiniGame or not Workspace.StaticMap.ice_cube_hill_minigame_minigame_state.is_game_active.Value
+                until not isInMiniGame
                 --until not isInMiniGame or count > 120
 
                 isInMiniGame = false
