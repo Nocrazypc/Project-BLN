9b5e20e6790a629b5ad66c214d6a14415ffcda00
Nocrazypc <nocrazypc@hotmail.com>
2025-04-07 05:50:41 -0300

    Update Bloss.lua

--- Bloss.lua
+++ Bloss.lua
@@ -32,7 +32,7 @@
              TextLabel.BorderSizePixel = 0
              TextLabel.Size = UDim2.new(1, 0, 1, 0)
              TextLabel.Font = Enum.Font.SourceSans
-             TextLabel.Text = "🍕"
+             TextLabel.Text = "🌸🌸🌸"
              TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
              TextLabel.TextScaled = true
              TextLabel.TextSize = 14
