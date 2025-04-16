d74313d93243293ad76a4578f124eb5df21e8a91
Nocrazypc <nocrazypc@hotmail.com>
2025-04-16 04:39:54 -0300

    Update CodeV4_Test.lua

--- Deleted Files/CodeV4_Test.lua
+++ Deleted Files/CodeV4_Test.lua
@@ -143,16 +143,11 @@ do
 
             correctBuild = true
 
-            if not warned then
-                warn('Rayfield | Build Mismatch')
-                print(
-[[Rayfield may encounter issues as you are running an incompatible interface version (]] .. ((Rayfield:FindFirstChild('Build') and Rayfield.Build.Value) or 'No Build') .. 
-[[).
-
-This version of Rayfield is intended for interface build ]] .. InterfaceBuild .. '.')
-
-                warned = true
-            end
+	--[[if not warned then
+		warn('Rayfield | Build Mismatch')
+		print('Rayfield may encounter issues as you are running an incompatible interface version ('.. ((Rayfield:FindFirstChild('Build') and Rayfield.Build.Value) or 'No Build') ..').\n\nThis version of Rayfield is intended for interface build '..InterfaceBuild..'.')
+		warned = true
+	end--]]
 
             toDestroy, Rayfield = Rayfield, useStudio and script.Parent:FindFirstChild('Rayfield') or game:GetObjects('rbxassetid://10804731440')[1]
 
