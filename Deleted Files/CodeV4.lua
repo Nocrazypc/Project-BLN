d768ac6d775167e4fe07b448855205a071399581
Nocrazypc <nocrazypc@hotmail.com>
2025-05-17 04:14:55 -0300

    Update CodeV4.lua

--- Deleted Files/CodeV4.lua
+++ Deleted Files/CodeV4.lua
@@ -7928,7 +7928,7 @@ local autoFarm = function()
     Teleport.PlaceFloorAtFarmingHome()
     Teleport.PlaceFloorAtCampSite()
     Teleport.PlaceFloorAtBeachParty()
-    Teleport.DownloadMainMap()
+    --Teleport.DownloadMainMap()
     Teleport.FarmingHome()
     task.delay(30, function()
         hasStartedFarming = true
