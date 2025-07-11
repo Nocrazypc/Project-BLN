cf7ae550d6daac021f10ee30610d0b2836a5c21a
Nocrazypc <nocrazypc@hotmail.com>
2025-07-11 04:28:16 -0300

    Update CodeV4.lua

--- Deleted Files/CodeV4.lua
+++ Deleted Files/CodeV4.lua
@@ -2727,7 +2727,7 @@ do
             end
 
             RouterClient.get('HousingAPI/SetDoorLocked'):InvokeServer(true)
-            Utils.PlaceFLoorUnderPlayer()
+            --Utils.PlaceFLoorUnderPlayer()
             RouterClient.get('TeamAPI/ChooseTeam'):InvokeServer('Babies', {
                 ['dont_send_back_home'] = true,
             })
