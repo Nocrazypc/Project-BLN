d832adaa2983daa986458e8cb108e83902dd9b43
Nocrazypc <nocrazypc@hotmail.com>
2025-07-19 20:21:29 -0300

    Update CodeV4.lua

--- Deleted Files/CodeV4.lua
+++ Deleted Files/CodeV4.lua
@@ -2749,10 +2749,10 @@ do
         end
         function SummerFest2025.OpenChests()
             local chestOpenedAmount = #playerData.tide_chest_manager.chests_opened
-            if chestOpenedAmount >= 6 then
+            if chestOpenedAmount >= 10 then
                 return
             end
-            for i = 1, 6 do
+            for i = 1, 10 do
                 pcall(function()
                     return RouterClient.get('SummerfestEventAPI/RequestOpenTideChest'):InvokeServer(i)
                 end)
