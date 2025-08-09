3e70bccad20fda3243df8174e69fa47cddb81d9e
Nocrazypc <nocrazypc@hotmail.com>
2025-08-08 21:16:42 -0300

    Update CodeV4.lua

--- Deleted Files/CodeV4.lua
+++ Deleted Files/CodeV4.lua
@@ -5198,7 +5198,7 @@ do
 
             setupFloor()
             CollisionsClient.set_collidable(false)
-            AztecEvent.StartEvent()
+            --AztecEvent.StartEvent()
             Teleport.FarmingHome()
             Utils.PrintDebug('teleported to farming place')
             Utils.PrintDebug('Started Farming')
