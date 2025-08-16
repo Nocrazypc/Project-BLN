45932f9468eb6ae184cef13521ce4bbe01b42f20
Nocrazypc <nocrazypc@hotmail.com>
2025-08-16 19:09:00 -0300

    Update CodeV4.lua

--- Deleted Files/CodeV4.lua
+++ Deleted Files/CodeV4.lua
@@ -5073,9 +5073,9 @@ do
                 elseif key == 'camping' then
                     Teleport.PlaceFloorAtCampSite()
                     Ailment.CampingAilment(petUnique)
-                    task.wait(4)
-                    Utils.ReEquipPet(Ailment.whichPet)
-                    task.wait(2)
+                    --task.wait(4)
+                    --Utils.ReEquipPet(Ailment.whichPet)
+                    task.wait(60)
                     Teleport.FarmingHome()
 
                     return true
