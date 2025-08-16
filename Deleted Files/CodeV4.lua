11543e2f0aead7b8337e0639ba9bd3582b1ac3da
Nocrazypc <nocrazypc@hotmail.com>
2025-08-16 08:51:59 -0300

    Update CodeV4.lua

--- Deleted Files/CodeV4.lua
+++ Deleted Files/CodeV4.lua
@@ -4361,7 +4361,7 @@ do
         function Ailment.CampingAilment(petUnique)
             Utils.PrintDebug(string.format('\u{1f3d5}\u{fe0f} Doing camping task on %s \u{1f3d5}\u{fe0f}', tostring(Ailment.whichPet)))
             Teleport.CampSite()
-            task.wait(6)
+            task.wait(2)
             Utils.ReEquipPet(Ailment.whichPet)
             waitForTaskToFinish('camping', petUnique)
         end
