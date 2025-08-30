bbc2980dc944735b7d8c94fb40b021be52266fdb
Nocrazypc <nocrazypc@hotmail.com>
2025-08-30 18:59:05 -0300

    Update CodeV4.lua

--- Deleted Files/CodeV4.lua
+++ Deleted Files/CodeV4.lua
@@ -3012,7 +3012,6 @@ do
         local DialogApp = (PlayerGui:WaitForChild('DialogApp'))
         local MinigameRewardsApp = (PlayerGui:WaitForChild('MinigameRewardsApp'))
         local MinigameInGameApp = (PlayerGui:WaitForChild('MinigameInGameApp'))
-        local BattlePassApp = (PlayerGui:WaitForChild('BattlePassApp'))
         local TradeApp = (PlayerGui:WaitForChild('TradeApp'))
         local certificateConn
         local starterPackAppConn
