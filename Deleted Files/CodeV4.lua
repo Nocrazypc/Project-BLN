de93a8391c4dfccea6fca6fdf5dbc513b98d47dc
Nocrazypc <nocrazypc@hotmail.com>
2025-09-12 16:09:59 -0300

    Update CodeV4.lua

--- Deleted Files/CodeV4.lua
+++ Deleted Files/CodeV4.lua
@@ -5068,11 +5068,7 @@ do
                         end
                     end
                     if --[[getgenv().SETTINGS.FOCUS_FARM_AGE_POTION or--]] getgenv().FocusFarmAgePotions then
-                         Taskboard:NewClaim()
-                         if not FarmingPet.GetTaskBoardPet(1) then
                          FarmingPet.GetPetToFarm(1)
-                         end
-                        task.wait(1)
                     end
                     if not completePetAilments(1) then
                         task.wait()
