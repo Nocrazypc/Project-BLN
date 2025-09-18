620202219342f80c884a08dc5f0a5160d542c98a
Nocrazypc <nocrazypc@hotmail.com>
2025-09-17 21:39:58 -0300

    Update CodeV4.lua

--- Deleted Files/CodeV4.lua
+++ Deleted Files/CodeV4.lua
@@ -4968,7 +4968,7 @@ do
             PetRelease.Use(GetInventory.GetPetsToRelease())
         end--]]
         local tryFeedAgePotion = function()
-            if not --[[getgenv().SETTINGS.FOCUS_FARM_AGE_POTION or--]] getgenv().FocusFarmAgePotions then
+            if not getgenv().FOCUS_FARM_AGE_POTION then
                 if ClientData.get('pet_char_wrappers')[1] and table.find(GetInventory.GetPetEggs(), ClientData.get('pet_char_wrappers')[1].pet_id) then
                     Utils.PrintDebug('is egg, not feeding age potion')
                 else
