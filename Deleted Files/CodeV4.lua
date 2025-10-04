fba4ca4189685029e565765af4226f07fcc61a5c
Nocrazypc <nocrazypc@hotmail.com>
2025-10-04 02:25:52 -0300

    Update CodeV4.lua

--- Deleted Files/CodeV4.lua
+++ Deleted Files/CodeV4.lua
@@ -7000,7 +7000,7 @@ getgenv().HatchPriorityEggs = false
 getgenv().ENABLE_RELEASE_PETS = false
 getgenv().PETS_TO_AGE_IN_PEN = {}
 
-getgenv().AutoMinigame = false
+getgenv().AutoMinigame = true
 getgenv().AutoMinigame2 = false
 
 local files = {
