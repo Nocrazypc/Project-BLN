2f56c7922150da5b362c15b4d00cfee620d9d8c1
Nocrazypc <nocrazypc@hotmail.com>
2025-10-23 07:17:27 -0300

    Update CodeV4.lua

--- Deleted Files/CodeV4.lua
+++ Deleted Files/CodeV4.lua
@@ -10318,11 +10318,11 @@ FarmTab:CreateSection("Events & Minigames: Nothing")
                     end
                     Ailment.BabyDirtyAilment(furniture.stylishshower)
                     return
-                --[[elseif key == 'scale_the_organ' then
+                elseif key == 'scale_the_organ' then
                     Ailment.ScaleTheOrgan()
                     task.wait(3)
                     Teleport.FarmingHome()
-                    return true--]]
+                    return true
 
 	            elseif key == 'popcorn' then
                     Ailment.Popcorn()
