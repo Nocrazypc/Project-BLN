f5407d5c42cabf86cd729521189fe6d4131a46d4
Nocrazypc <nocrazypc@hotmail.com>
2025-11-03 15:06:50 -0300

    Update CodeV4.lua

--- Deleted Files/CodeV4.lua
+++ Deleted Files/CodeV4.lua
@@ -9765,7 +9765,6 @@ FarmTab:CreateSection("Events & Minigames: Nothing")
             end)
             waitForTaskToFinish('buccaneer_band', petUnique)
             getUpFromSitting()
-            end
         end
         function Ailment.BabyHungryAilment()
             Utils.PrintDebug('\u{1f476}\u{1f374} Doing baby hungry task \u{1f476}\u{1f374}')
