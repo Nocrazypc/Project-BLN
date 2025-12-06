19a9c621464184e26701d1d03c7202828b3cc1a1
Nocrazypc <nocrazypc@hotmail.com>
2025-12-05 23:00:26 -0300

    Update CodeV4.lua

--- Deleted Files/CodeV4.lua
+++ Deleted Files/CodeV4.lua
@@ -8331,7 +8331,7 @@ Check the Developer Console for more information.]],
 ------------- Rayfield Config ------------- 
         local setupRayfield = function()
         local Window = Rayfield:CreateWindow({
-	        Name = "BLN Adopt Me! Autofarm V4.4  😎 🦇 🧛‍♂️ 🎃",
+	        Name = "BLN Adopt Me! Autofarm V4.4  😎",
                 Theme = 'Default',
                 DisableRayfieldPrompts = true,
                 DisableBuildWarnings = true,
