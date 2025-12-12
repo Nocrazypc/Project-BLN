d5be2838e3c235a32a36a8b2813f789f98d96aba
Nocrazypc <nocrazypc@hotmail.com>
2025-12-12 18:56:50 -0300

    Update CodeV4.lua

--- Deleted Files/CodeV4.lua
+++ Deleted Files/CodeV4.lua
@@ -10938,7 +10938,7 @@ for index, _table in ipairs(files)do
 end
 
 -------------- Autostart some toggled options for Feli---------------------
-task.wait(15)
+task.wait(60)
 -------- Low Render- Hide parts ------------
 task.spawn(function()
         
