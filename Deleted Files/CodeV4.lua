f7c6d95ecdf9e37641fdbb6448885ffd7721e6d8
Nocrazypc <nocrazypc@hotmail.com>
2025-12-26 21:48:12 -0300

    Update CodeV4.lua

--- Deleted Files/CodeV4.lua
+++ Deleted Files/CodeV4.lua
@@ -1805,8 +1805,7 @@ do
 
             Workspace.Interiors['Neighborhood!Fall']:WaitForChild('InteriorOrigin')
 
-            localPlayer.Character.PrimaryPart.CFrame = Workspace.Interiors['Neighborhood!Fall'].InteriorOrigin.CFrame + Vector3.new(0, 
--10, 0)
+            localPlayer.Character.PrimaryPart.CFrame = Workspace.Interiors['Neighborhood!Fall'].InteriorOrigin.CFrame + Vector3.new(0, 10, 0)
             localPlayer.Character:WaitForChild('HumanoidRootPart').Anchored = false
 
             localPlayer.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Landed)
