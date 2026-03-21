718aef493a923912810dad5ae913f22cbbac3139
Nocrazypc <nocrazypc@hotmail.com>
2026-03-21 01:07:58 -0300

    Update CodeV4.lua

--- Deleted Files/CodeV4.lua
+++ Deleted Files/CodeV4.lua
@@ -3396,7 +3396,7 @@ do
 
         local patterns = {
             ['UH OH!'] = 'Okay',
-            ['is starting soon!'] = 'Yes',
+            ['Waffle Scuffle is starting soon!'] = 'No',
             ['Would you like to teleport'] = 'No',
             ["You haven't collected the Gingerbread"] = 'No',
             ['Be careful when trading'] = 'Okay',
