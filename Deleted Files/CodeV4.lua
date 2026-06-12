6456831aa157ee7278da95570105d77c453f51c9
Nocrazypc <nocrazypc@hotmail.com>
2026-06-12 09:43:32 -0300

    Update CodeV4.lua

--- Deleted Files/CodeV4.lua
+++ Deleted Files/CodeV4.lua
@@ -3396,8 +3396,7 @@ do
         }
 
         local patterns = {
-            ['Thunder has'] = 'Okay',
-            ['Boat tour'] = 'Okay',
+            ['Boat tour starts'] = 'Okay',
             ['Would you like to teleport'] = 'No',
             ["You haven't collected the Gingerbread"] = 'No',
             ['Be careful when trading'] = 'Okay',
