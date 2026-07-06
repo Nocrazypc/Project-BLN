7133cc839fc787db50f0c23cb72b5393a63d707e
Nocrazypc <nocrazypc@hotmail.com>
2026-07-06 00:53:44 -0300

    Update CodeV4.lua

--- Deleted Files/CodeV4.lua
+++ Deleted Files/CodeV4.lua
@@ -4540,7 +4540,7 @@ do
                 BuyItem.StartBuyItems({
                     {
                         NameId = 'summer_2026_storm_condor',
-                        MaxAmount = 20,
+                        MaxAmount = 10,
                     },
                 })
             end
