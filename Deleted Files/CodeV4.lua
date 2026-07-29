2d49bac09ee6c767c028f760556b94479d8fcb80
Nocrazypc <nocrazypc@hotmail.com>
2026-07-29 07:05:16 -0300

    Update CodeV4.lua

--- Deleted Files/CodeV4.lua
+++ Deleted Files/CodeV4.lua
@@ -4470,9 +4470,7 @@ do
             function BeePass2026.Init() end
             function BeePass2026.Start()
 
-              -- if getgenv().BeePass2026 then
                task.spawn(infLoop)
-              -- end
 
             end
 
